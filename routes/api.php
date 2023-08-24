d15c90b74586b834a0f46f4bb6bcb1c864d5851e
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-08-24 01:38:58 +0100

    api logout error fixed - moved logout route to protected

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,11 @@ use Illuminate\Support\Facades\Route;
 // Auth Routes
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
