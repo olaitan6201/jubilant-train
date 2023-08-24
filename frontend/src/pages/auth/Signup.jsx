8ac3d2fab3f4d18e133f1d3a8f6a08879bfe236c
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-08-24 01:24:32 +0100

    completed Sign Up page component

--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import axiosClent from '../../clients/axios-client'
+import { useStateContext } from '../../contexts/ContextProvider'
+import Errors from '../../components/layouts/shared/Errors'
 
 export default function Signup() {
-    const onSubmit = (e) => {
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmationRef = useRef()
+
+    const [errors, setErrors] = useState(null)
+
+    const { setUser, setToken } = useStateContext()
+
+    const onSubmit = async (e) => {
         e.preventDefault()
+
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        }
+
+        try {
+            const res = await axiosClent.post('/signup', payload);
+            setUser(res?.data?.data?.user)
+            setToken(res?.data?.data?.token)
+        } catch (error) {
+            const response = error.response
+            if (response && response.status === 422) {
+                setErrors(response.data.errors)
+            }
+        }
+
     }
 
     return (
@@ -13,12 +44,13 @@ export default function Signup() {
                     <h1 className='title'>
                         Sign up for free
                     </h1>
-                    <input type='text' placeholder='Full Name' />
-                    <input type='email' placeholder='Email Address' />
-                    <input type='password' placeholder='Password' />
-                    <input type='password' placeholder='Password Confirmation' />
+                    <Errors errors={errors} />
+                    <input ref={nameRef} type='text' placeholder='Full Name' />
+                    <input ref={emailRef} type='email' placeholder='Email Address' />
+                    <input ref={passwordRef} type='password' placeholder='Password' />
+                    <input ref={passwordConfirmationRef} type='password' placeholder='Password Confirmation' />
                     <button type='submit' className='btn btn-block'>
-                        Signup
+                        Sign Up
                     </button>
                     <p className='message'>
                         Already Registered? &nbsp;
