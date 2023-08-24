a2a978c5b71179a8b1ad4d7319aa55bd4e0f608a
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-08-24 15:29:21 +0100

    updated router

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -6,6 +6,7 @@ import NotFound from './pages/errors/NotFound'
 import GuestLayout from './components/layouts/GuestLayout'
 import AppLayout from './components/layouts/AppLayout'
 import Dashboard from './pages/Dashboard'
+import UserForm from './pages/UserForm'
 
 const router = createBrowserRouter([
     {
@@ -23,6 +24,14 @@ const router = createBrowserRouter([
             {
                 path: '/users',
                 element: <Users />
+            },
+            {
+                path: '/users/new',
+                element: <UserForm key={"userCreate"} />
+            },
+            {
+                path: '/users/:id',
+                element: <UserForm key={"userUpdate"} />
             },
         ]
     },
